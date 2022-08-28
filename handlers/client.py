152c87574391ce40c57d709e427d73edb489854b
Evgeny Shamray <EvSha83@yandex.ru>
2022-08-28 19:14:51 +0300

    "Add func get answer and inline button 'Посмотреть ответ' in client module and inline button 'Отменить' in module loading question "

--- handlers/client.py
+++ handlers/client.py
@@ -1,7 +1,12 @@
+import random
+
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import filters
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from api import get_questions, get_data_by_id
 from create_bot import bot
+from create_bot import dp
 from key_boards import kb_client
 
 
@@ -13,21 +18,29 @@ async def command_start(message: types.Message):
 # @dp.message_handler(commands=['help'])
 async def show_help_text(message: types.Message):
     await bot.send_message(message.from_user.id, '''Данный бот предназначен для повторения часто встречающихся вопросов на собеседовании по теме "Тестирование программного продукта". 
-По нажатию кнопки "Получить вопросы" бот отправит вопросы  и ответы на них. Нажав на кнопку "Загрузить свой вопрос", можно загрузить вопрос с ответом для пополнения базы знаний бот ''',
-                           reply_markup=kb_client)
+По нажатию кнопки "Получить вопросы" бот отправит вопросы  и ответы на них. Нажав на кнопку "Загрузить свой вопрос", можно загрузить вопрос с ответом для пополнения базы знаний бот '''
+                           )
 
 
 # @dp.register_message_handler(commands=['get_questions_and_answers'])
 async def get_questions_and_answers(message: types.Message):
-    await bot.send_message(message.from_user.id, "Получить вопросы", reply_markup=kb_client)
+    all_questions = await get_questions()
+    new_list = random.sample(all_questions, 5)
+    for question in new_list:
+        await bot.send_message(message.from_user.id, f'{question["title"]}', reply_markup=InlineKeyboardMarkup(resize_keyboard=True). \
+                               add(
+            InlineKeyboardButton('Посмотреть ответ', callback_data=f'show_answer {question["id"]}')))
 
 
-
-
+@dp.callback_query_handler(lambda x: x.data and x.data.startswith('show_answer'))
+async def show_answer(call: types.CallbackQuery):
+    print(call.message)
+    data = await get_data_by_id(int(call.data.strip('show_answer ')))
+    # await bot.send_message(call.message.from_user.id, data['answer'], reply_to_message_id=call.inline_message_id)
+    await call.message.edit_text(f'Вопрос: \n*{data["title"]}*\n**Ответ:** \n_{data["answer"]}_', parse_mode='Markdown')
 
 def register_handler_client(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=['start'])
     dp.register_message_handler(show_help_text, commands=['help'])
     dp.register_message_handler(show_help_text, filters.Text(equals=['Помощь']))
     dp.register_message_handler(get_questions_and_answers, filters.Text(equals=['Получить вопросы и ответы']))
-
--- handlers/loading_question.py
+++ handlers/loading_question.py
@@ -1,11 +1,13 @@
 from aiogram import types,Dispatcher
 from aiogram.dispatcher.filters import Text
 from key_boards.client_kb import kb_client
+from api import set_questions
 
 
 from create_bot import dp, bot
 from aiogram.dispatcher import FSMContext, filters
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 class FSMAddQuestion(StatesGroup):
     question = State()
@@ -15,20 +17,18 @@ class FSMAddQuestion(StatesGroup):
 # Начало диалога о загрузки вопроса и ответа
 # @dp.register_message_handler(filters.Text(equals=['Добавить вопросы и ответы']),state=None)
 async def set_questions_and_answers(message: types.Message):
-    await bot.send_message(message.from_user.id,'Загрузите свой вопрос', reply_markup=kb_client)
+    await bot.send_message(message.from_user.id,'Загрузите свой вопрос', reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('Отменить', callback_data='cancel')))
     await FSMAddQuestion.question.set()
 
 
-
-# Выход по кнопке "Отмена" из машинного состояния
-# @dp.message_handler(state='*',commands='отмена')
-# @dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
-async def cancel_handler(message: types.Message, state: FSMContext):
-    current_state=await state.get_state()
+@dp.callback_query_handler(text="cancel",state='*')
+async def cancel_callback(call:types.CallbackQuery, state: FSMContext):
+    current_state = await state.get_state()
     if current_state is None:
         return
     await state.finish()
-    await message.reply('OK')
+    await call.message.reply('Загрузка отменена')
+
 
 
 # Запись вопроса пользователя
@@ -36,18 +36,18 @@ async def cancel_handler(message: types.Message, state: FSMContext):
 async  def load_question(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['question'] = message.text
+        data['user_id'] = message.from_user.id
     await FSMAddQuestion.next()
     await message.reply('Загрузите ответ на свой вопрос')
 
 
 # Запись вопроса пользователя
 # @dp.message_handler(state=FSMAddQuestion.answer)
-async  def load_answer(message: types.Message, state: FSMContext):
+async def load_answer(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['answer'] = message.text
-    # TODO Запись данных пользователя в БД
-    async with state.proxy() as data:
-        await message.reply(str(data))
+        status = await set_questions(data['question'],data['answer'],data['user_id'])
+        print(status)
     await state.finish()
 
 
@@ -55,8 +55,6 @@ async  def load_answer(message: types.Message, state: FSMContext):
 def register_handler(dp:Dispatcher):
     dp.register_message_handler(set_questions_and_answers, filters.Text(equals=['Добавить вопросы и ответы']),
                                 state=None)
-    dp.register_message_handler(cancel_handler, state='*', commands='отмена')
-    dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state='*')
     dp.register_message_handler(load_question, state=FSMAddQuestion.question)
     dp.register_message_handler(load_answer, state=FSMAddQuestion.answer)
 
