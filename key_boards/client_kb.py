eb8dbfaef97ea606a8ef9d4733afef57a4b67162
Evgeny Shamray <EvSha83@yandex.ru>
2022-08-28 19:10:52 +0300

    "Change client keyboard"

--- key_boards/client_kb.py
+++ key_boards/client_kb.py
@@ -3,7 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 b1 = KeyboardButton('Получить вопросы и ответы')
 b2 = KeyboardButton('Добавить вопросы и ответы')
-b3 = KeyboardButton('Помощь')
+
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
-kb_client.add(b3).row(b1,b2)
+kb_client.row(b1,b2)
